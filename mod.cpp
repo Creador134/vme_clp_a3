d1ebff1b5acfa5566f55a136d49d3d188bb529e0
Hu Hao <huhao@outlook.com>
2013-03-09 18:46:34 +0800

    add mod infomation

---
+++ mod.cpp
@@ -0,0 +1,4 @@
+   name = "VME Chinese Language Pack";
+   // picture = "mod.paa"; optional to use different mod picture, mod.paa is default
+   actionName = "Website";
+   action = "http://www.chinavme.com/"; //only websites are currently possible here
